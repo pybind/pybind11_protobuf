5e5a9129ecf5066171b38ace8bbd3423c27219b6
Laramie Leavitt <lar@google.com>
2021-06-22 13:01:57 -0700

    Internal change
    PiperOrigin-RevId: 380874691

---
+++ pybind11_protobuf/tests/dynamic_message_module.cc
@@ -0,0 +1,138 @@
+// Copyright (c) 2021 The Pybind Development Team. All rights reserved.
+//
+// All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+#include <pybind11/pybind11.h>
+
+#include <functional>
+#include <memory>
+#include <stdexcept>
+
+#include "net/proto2/contrib/parse_proto/parse_text_proto.h"
+#include "google/protobuf/descriptor.pb.h"
+#include "google/protobuf/descriptor.h"
+#include "google/protobuf/dynamic_message.h"
+#include "google/protobuf/message.h"
+#include "pybind11_protobuf/fast_cpp_proto_casters.h"
+#include "pybind11_protobuf/tests/test.pb.h"
+
+namespace py = ::pybind11;
+
+namespace pybind11_test {
+
+// GetDynamicPool contains a dynamic message that is wire-compatible with
+// with IntMessage; conversion using the fast_cpp_proto api will fail
+// as the PyProto_API will not be able to find the proto in the default
+// pool.
+::google::protobuf::DescriptorPool* GetDynamicPool() {
+  static ::google::protobuf::DescriptorPool* pool = [] {
+    ::google::protobuf::FileDescriptorProto file_proto =
+        ::google::protobuf::contrib::parse_proto::ParseTextProtoOrDie(R"pb(
+          name: 'pybind11_protobuf/tests'
+          package: 'pybind11.test'
+          message_type: {
+            name: 'DynamicMessage'
+            field: { name: 'value' number: 1 type: TYPE_INT32 }
+          }
+          message_type: {
+            name: 'IntMessage'
+            field: { name: 'value' number: 1 type: TYPE_INT32 }
+          }
+        )pb");
+
+    ::google::protobuf::DescriptorPool* pool = new ::google::protobuf::DescriptorPool();
+    pool->BuildFile(file_proto);
+    return pool;
+  }();
+
+  return pool;
+}
+
+void UpdateMessage(::google::protobuf::Message* message, int32 value) {
+  auto* f = message->GetDescriptor()->FindFieldByName("value");
+  if (!f) f = message->GetDescriptor()->FindFieldByName("value_int32");
+  message->GetReflection()->SetInt32(message, f, value);
+}
+
+bool CheckMessage(const ::google::protobuf::Message& message, int32 value) {
+  auto* f = message.GetDescriptor()->FindFieldByName("value");
+  if (!f) f = message.GetDescriptor()->FindFieldByName("value_int32");
+  return message.GetReflection()->GetInt32(message, f) == value;
+}
+
+std::unique_ptr<::google::protobuf::Message> GetDynamicMessage(const std::string& full_name,
+                                                   int32_t value) {
+  static ::google::protobuf::DynamicMessageFactory factory(GetDynamicPool());
+
+  auto* descriptor = GetDynamicPool()->FindMessageTypeByName(full_name);
+  if (!descriptor) return nullptr;
+
+  auto* prototype = factory.GetPrototype(descriptor);
+  if (!prototype) return nullptr;
+
+  std::unique_ptr<::google::protobuf::Message> dynamic(prototype->New());
+  UpdateMessage(dynamic.get(), value);
+  return dynamic;
+}
+
+PYBIND11_MODULE(dynamic_message_module, m) {
+  m.def(
+      "dynamic_message_ptr",
+      [](std::string name, int32_t value) -> ::google::protobuf::Message* {
+        return GetDynamicMessage(name, value).release();
+      },
+      py::arg("name") = "pybind11.test.DynamicMessage", py::arg("value") = 123);
+
+  m.def(
+      "dynamic_message_unique_ptr",
+      [](std::string name, int32_t value) -> std::unique_ptr<::google::protobuf::Message> {
+        return GetDynamicMessage(name, value);
+      },
+      py::arg("name") = "pybind11.test.DynamicMessage", py::arg("value") = 123);
+
+  m.def(
+      "dynamic_message_shared_ptr",
+      [](std::string name, int32_t value) -> std::shared_ptr<::google::protobuf::Message> {
+        return GetDynamicMessage(name, value);
+      },
+      py::arg("name") = "pybind11.test.DynamicMessage", py::arg("value") = 123);
+
+  m.def("check_message", &CheckMessage, py::arg("message"), py::arg("value"));
+  m.def(
+      "check_message_const_ptr",
+      [](const ::google::protobuf::Message* m, int value) {
+        return (m == nullptr) ? false : CheckMessage(*m, value);
+      },
+      py::arg("message"), py::arg("value"));
+
+#if PYBIND11_PROTOBUF_UNSAFE
+  m.def(
+      "mutate_message",
+      [](::google::protobuf::Message* msg, int value) { UpdateMessage(msg, value); },
+      py::arg("message"), py::arg("value"));
+
+  m.def(
+      "mutate_message_ref",
+      [](::google::protobuf::Message& msg, int value) { UpdateMessage(&msg, value); },
+      py::arg("message"), py::arg("value"));
+#endif  // PYBIND11_PROTOBUF_UNSAFE
+
+  // overloaded functions
+  m.def(
+      "fn_overload", [](const ::google::protobuf::Message&) -> int { return 1; },
+      py::arg("message"));
+  m.def(
+      "fn_overload", [](const pybind11::test::IntMessage&) -> int { return 2; },
+      py::arg("message"));
+
+  // copies
+  m.def(
+      "roundtrip",
+      [](const ::google::protobuf::Message& inout) -> const ::google::protobuf::Message& {
+        return inout;
+      },
+      py::arg("message"), py::return_value_policy::copy);
+}
+
+}  // namespace pybind11_test
---
+++ pybind11_protobuf/tests/dynamic_message_test.py
@@ -0,0 +1,118 @@
+# Copyright (c) 2021 The Pybind Development Team. All rights reserved.
+#
+# All rights reserved. Use of this source code is governed by a
+# BSD-style license that can be found in the LICENSE file.
+"""Tests for protobuf casters."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from google.protobuf import descriptor_pb2
+from google.protobuf import descriptor_pool
+from google.protobuf import message_factory
+from google3.testing.pybase import googletest
+from google3.testing.pybase import parameterized
+from pybind11_protobuf.tests import compare
+from pybind11_protobuf.tests import dynamic_message_module as m
+from pybind11_protobuf.tests import test_pb2
+
+POOL = descriptor_pool.DescriptorPool()
+FACTORY = message_factory.MessageFactory(POOL)
+POOL.Add(
+    descriptor_pb2.FileDescriptorProto(
+        name='pybind11_protobuf/tests',
+        package='pybind11.test',
+        message_type=[
+            descriptor_pb2.DescriptorProto(
+                name='DynamicMessage',
+                field=[
+                    descriptor_pb2.FieldDescriptorProto(
+                        name='value', number=1, type=5)
+                ]),
+            descriptor_pb2.DescriptorProto(
+                name='IntMessage',
+                field=[
+                    descriptor_pb2.FieldDescriptorProto(
+                        name='value', number=1, type=5)
+                ])
+        ]))
+
+
+def get_py_dynamic_message(value=5):
+  """Returns a dynamic message that is wire-compatible with IntMessage."""
+  prototype = FACTORY.CreatePrototype(
+      POOL.FindMessageTypeByName('pybind11.test.DynamicMessage'))
+  msg = prototype(value=value)
+  return msg
+
+
+def get_py_dynamic_int_message(value=5):
+  """Returns a dynamic message named pybind11.test.IntMessage."""
+  prototype = FACTORY.CreatePrototype(
+      POOL.FindMessageTypeByName('pybind11.test.IntMessage'))
+  msg = prototype(value=value)
+  return msg
+
+
+def get_cpp_dynamic_message(value=5):
+  """Returns a dynamic message that is wire-compatible with IntMessage."""
+  return m.dynamic_message_ptr('pybind11.test.DynamicMessage', value)
+
+
+def get_cpp_dynamic_int_message(value=5):
+  """Returns a dynamic message named pybind11.test.IntMessage."""
+  return m.dynamic_message_ptr('pybind11.test.IntMessage', value)
+
+
+class ProtoTest(parameterized.TestCase, compare.ProtoAssertions):
+
+  @parameterized.named_parameters(
+      ('native_proto', test_pb2.IntMessage),
+      ('py_dynamic_int', get_py_dynamic_int_message),
+      ('cpp_dynamic_int', get_cpp_dynamic_int_message))
+  def test_full_name(self, get_message_function):
+    message = get_message_function()
+    self.assertEqual(
+        str(message.DESCRIPTOR.full_name), 'pybind11.test.IntMessage')
+
+  @parameterized.named_parameters(
+      ('native_proto', test_pb2.IntMessage),
+      ('py_dynamic_int', get_py_dynamic_int_message),
+      ('cpp_dynamic_int', get_cpp_dynamic_int_message),
+      #  ('py_dynamic', get_py_dynamic_message),
+      #  ('cpp_dynamic', get_cpp_dynamic_message),
+  )
+  def test_check_message(self, get_message_function):
+    message = get_message_function(value=5)
+    self.assertTrue(m.check_message(message, 5))
+    self.assertTrue(m.check_message_const_ptr(message, 5))
+
+  @parameterized.named_parameters(
+      ('py_dynamic_int', get_py_dynamic_int_message),
+      ('cpp_dynamic_int', get_cpp_dynamic_int_message),
+      #  ('py_dynamic', get_py_dynamic_message),
+      #  ('cpp_dynamic', get_cpp_dynamic_message),
+      #  ('cpp_dynamic_message_shared_ptr', m.dynamic_message_shared_ptr),
+      #  ('cpp_dynamic_unique_ptr', m.dynamic_message_unique_ptr),
+  )
+  def test_overload(self, get_message_function):
+    self.assertEqual(1, m.fn_overload(get_message_function()))
+
+  @parameterized.named_parameters(
+      ('native_proto', test_pb2.IntMessage),
+      ('py_dynamic_int', get_py_dynamic_int_message),
+      ('cpp_dynamic_int', get_cpp_dynamic_int_message),
+      #  ('py_dynamic', get_py_dynamic_message),
+      #  ('cpp_dynamic', get_cpp_dynamic_message),
+      #  ('cpp_dynamic_message_shared_ptr', m.dynamic_message_shared_ptr),
+      #  ('cpp_dynamic_unique_ptr', m.dynamic_message_unique_ptr),
+  )
+  def test_roundtrip(self, get_message_function):
+    a = get_message_function(value=6)
+    b = m.roundtrip(a)
+    self.assertTrue(m.check_message(b, 6))
+
+
+if __name__ == '__main__':
+  googletest.main()
