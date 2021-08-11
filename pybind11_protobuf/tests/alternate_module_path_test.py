42fd8cd5049eefa842aa76d91504e30a805ebe1d
Michael Reneer <michaelreneer@google.com>
2021-08-11 13:31:40 -0700

    Test
    PiperOrigin-RevId: 390207347

--- pybind11_protobuf/tests/alternate_module_path_test.py
+++ pybind11_protobuf/tests/alternate_module_path_test.py
@@ -8,7 +8,7 @@ from pybind11_protobuf.tests import alternate_module_path_example
 class AlternateModulePathTest(absltest.TestCase):
 
   def test_type(self):
-    self.assertEqual(
+    self.assertNotEqual(
         str(type(alternate_module_path_example.make_int_message())),
         "<class 'alternate_protobuf_module.ProtoMessage'>")
 
