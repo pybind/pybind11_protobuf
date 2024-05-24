1d7a7296604537db5f6ace2ddafd1d08c967ec63
Jan Kuehle <jankuehle@google.com>
2024-05-24 00:40:40 -0700

    Add and fix Bazel tests
    PiperOrigin-RevId: 636821161

--- scripts/build_and_run_tests.sh
+++ scripts/build_and_run_tests.sh
@@ -21,32 +21,8 @@ echo "Building and testing in $PWD using 'python' (version $PYVERSION)."
 
 bazel clean --expunge # Force a dep update
 
-# Currently only a subset of tests build.  The next one to fix is protobuf_test
-BAZEL_CXXOPTS="-std=c++17" bazel test --test_output=errors \
-    pybind11_protobuf/tests:extension_test \
-    pybind11_protobuf/tests:message_test \
-    pybind11_protobuf/tests:pass_by_test \
-    pybind11_protobuf/tests:proto_enum_test \
-    pybind11_protobuf/tests:wrapped_proto_module_test --enable_bzlmod
+BAZEL_CXXOPTS="-std=c++17" bazel test ... --test_output=errors "$@" --enable_bzlmod
+BAZEL_CXXOPTS="-std=c++20" bazel test ... --test_output=errors "$@" --enable_bzlmod
 
-BAZEL_CXXOPTS="-std=c++20" bazel test --test_output=errors \
-    pybind11_protobuf/tests:extension_test \
-    pybind11_protobuf/tests:message_test \
-    pybind11_protobuf/tests:pass_by_test \
-    pybind11_protobuf/tests:proto_enum_test \
-    pybind11_protobuf/tests:wrapped_proto_module_test --enable_bzlmod
-
-# Currently only a subset of tests build.  The next one to fix is protobuf_test
-BAZEL_CXXOPTS="-std=c++17" bazel test --test_output=errors \
-    pybind11_protobuf/tests:extension_test \
-    pybind11_protobuf/tests:message_test \
-    pybind11_protobuf/tests:pass_by_test \
-    pybind11_protobuf/tests:proto_enum_test \
-    pybind11_protobuf/tests:wrapped_proto_module_test --noenable_bzlmod
-
-BAZEL_CXXOPTS="-std=c++20" bazel test --test_output=errors \
-    pybind11_protobuf/tests:extension_test \
-    pybind11_protobuf/tests:message_test \
-    pybind11_protobuf/tests:pass_by_test \
-    pybind11_protobuf/tests:proto_enum_test \
-    pybind11_protobuf/tests:wrapped_proto_module_test --noenable_bzlmod
+BAZEL_CXXOPTS="-std=c++17" bazel test ... --test_output=errors "$@" --noenable_bzlmod
+BAZEL_CXXOPTS="-std=c++20" bazel test ... --test_output=errors "$@" --noenable_bzlmod
