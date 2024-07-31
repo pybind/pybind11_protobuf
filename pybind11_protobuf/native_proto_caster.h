a99aa53a79e64dd684991dcdb4b6234c99edd13c
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2024-07-31 07:44:40 -0700

    hg_testing_tip 2024-07-31+0731 (Dummy file.)
    PiperOrigin-RevId: 658004116

--- pybind11_protobuf/native_proto_caster.h
+++ pybind11_protobuf/native_proto_caster.h
@@ -91,6 +91,20 @@ struct type_caster<
     : public pybind11_protobuf::proto_caster<
           ProtoType, pybind11_protobuf::native_cast_impl> {};
 
+#if defined(PYBIND11_HAVE_INTERNALS_WITH_SMART_HOLDER_SUPPORT)
+
+template <typename ProtoType>
+struct copyable_holder_caster_shared_ptr_with_smart_holder_support_enabled<
+    ProtoType, enable_if_t<std::is_base_of<::google::protobuf::Message, ProtoType>::value>>
+    : std::false_type {};
+
+template <typename ProtoType>
+struct move_only_holder_caster_unique_ptr_with_smart_holder_support_enabled<
+    ProtoType, enable_if_t<std::is_base_of<::google::protobuf::Message, ProtoType>::value>>
+    : std::false_type {};
+
+#endif  // PYBIND11_HAVE_INTERNALS_WITH_SMART_HOLDER_SUPPORT
+
 // NOTE: If smart_holders becomes the default we will need to change this to
 //    type_caster<std::unique_ptr<ProtoType, D>, ...
 // Until then using that form is ambiguous due to the existing specialization
